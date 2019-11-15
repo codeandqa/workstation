4e8063b9956a9eb20a7c65645eafa01f602412ce
aditya-CodeAndQA <aditya@anwisys.com>
2019-11-15 15:53:54 +0000

    refactor

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -11,10 +11,9 @@ end
 
 package 'ntp'
 
-file '/etc/motd' do
-	content 'This server is the property of ...'
-	action :create
-	owner 'root'
-	group 'root'
+
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create 
 end
 
