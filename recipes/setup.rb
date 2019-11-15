2db15b6c15e2aa87ebfa8c4dd4da2280a4022f8c
aditya-CodeAndQA <aditya@anwisys.com>
2019-11-15 14:00:25 +0000

    initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,20 @@
+package 'vim-enhanced'
+package 'nano'
+
+package 'tree' do
+	action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+	content 'This server is the property of ...'
+	action :create
+	owner 'root'
+	group 'root'
+end
+
